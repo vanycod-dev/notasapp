617d06ed156a28e89f7a49609ff1e6c673aea4a8
Jhovan <perjhoal@gmail.com>
2025-04-17 10:16:06 -0600

    Comitenado bac

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,1 +1,26 @@
-a
+const pool = require('./db');
+
+// Crear usuario
+const crearUsuario = async (usuario, email, password) => {
+    const [result] = await pool.query(
+        'INSERT INTO usuarios  (usuario, email, password) values (?, ?, ?)',
+        [usuario, email, password]
+    );
+    return result.insertId;
+}
+
+// Buscar usuario
+
+const getUsuario = async (usuario) => {
+    const [result] = await pool.query(
+        'SELECT * FROM usuario WHERE usuario = ?',
+        [usuario]
+    );
+    return result;
+}
+
+
+module.exports = {
+  crearUsuario,
+  getUsuario
+};
